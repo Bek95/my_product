e07ba17381a70bc93e8c11df42fd22b708a11dd8
bekkai <bekkai.boulerhcha@tsi-payment.com>
2020-07-01 17:03:29 +0200

    creation model, migration and pivot table

---
+++ application/database/migrations/2020_07_01_143806_create_articles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('color');
+            $table->string('kind');
+            $table->string('size');
+            $table->string('description');
+            $table->float('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
