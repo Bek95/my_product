fc11677e64d8255d6a2599d58b4b36243e965060
bekkai <bekkai.boulerhcha@tsi-payment.com>
2020-07-01 18:58:55 +0200

    check if i can get category's articles

--- application/app/Http/Controllers/MainController.php
+++ application/app/Http/Controllers/MainController.php
@@ -3,16 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Article;
+use App\Category;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function home()
     {
-        //ici, je vérifie si je récupère bien les catégories de l'article qui a pour id 10
-        $article = Article::find(10);
-        $categories = $article->categories;
-        dd($categories);
+        //ici je vérifie le nombre d'article qui appartient a la catégorie qui a pour id 1
+        $category = Category::find(1);
+        $articles = $category->articles;
+
+        dd($articles);
         return view('main.home');
     }
 }
